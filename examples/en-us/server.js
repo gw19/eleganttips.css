8f05bc0b72582b3f4de49e2ab7e43048837a3d20
GW19 <imgw19@gmail.com>
2017-11-30 07:49:49 +0800

    Modified example pages.

--- examples/en-us/server.js
+++ examples/en-us/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 let app = express();
-let port = process.env.PORT || 3232;
+let port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/img'));
+app.use(express.static(__dirname + '/../../'));
 app.listen(port, () => {
   console.log('Server listening at port %d', port);
 });
