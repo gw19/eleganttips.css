f7459661c8b8e655f179b85d70e9a74df0a8add3
GW19 <imgw19@gmail.com>
2017-12-05 13:56:39 +0800

    Eleganttips official website

--- examples/en-us/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-let app = express();
-let port = process.env.PORT || 3000;
-app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/img'));
-app.use(express.static(__dirname + '/../../'));
-app.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
